f5564bee6aefd88924f61786ff600568c80ed523
hawanha1 <bsef18m047@pucit.edu.pk>
2021-04-11 05:54:57 +0500

    animation code

---
+++ lecture-9/app/src/main/java/org/haqnawaz/mclecture9/AudioActivity.java
@@ -0,0 +1,28 @@
+package org.haqnawaz.mclecture9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class AudioActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio);
+        mp = MediaPlayer.create(this,R.raw.a_sound);
+    }
+
+    public void Play (View view){
+        mp.start();
+    }
+
+    public void Pause (View view){
+        mp.pause();
+    }
+}
+
---
+++ lecture-9/app/src/main/java/org/haqnawaz/mclecture9/MainActivity.java
@@ -0,0 +1,33 @@
+package org.haqnawaz.mclecture9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Animation (View view){
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+        //imageView.animate().alpha(0).setDuration(500);
+
+        // imageView.animate().translationXBy(-500).setDuration(500);
+
+        // imageView.animate().rotation(720).setDuration(2000);
+
+        // imageView.animate().rotation(720).alpha(0).setDuration(2000);
+
+        // imageView.animate().scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+
+        imageView.animate().rotation(720).scaleX(0.5f).scaleY(0.5f).setDuration(2000);
+    }
+}
