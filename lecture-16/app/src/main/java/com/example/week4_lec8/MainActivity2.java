e1cb04cabb329a82c2bffce753e09247c0460ba0
hawanha1 <bsef18m047@pucit.edu.pk>
2021-05-18 06:21:19 +0500

    intent code

---
+++ lecture-16/app/src/main/java/com/example/week4_lec8/MainActivity2.java
@@ -0,0 +1,55 @@
+package com.example.week4_lec8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity2 extends AppCompatActivity {
+    String TAG = "MainActivity2";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Log.d(TAG, "onCreate: ");
+    }
+
+    public void LaunchActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: ");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: ");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: ");
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: ");
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: ");
+        super.onResume();
+    }
+}
