bf638b6695feecf04e42e802c946e63b94c67408
hawanha1 <bsef18m047@pucit.edu.pk>
2021-05-24 06:35:25 +0500

    fragments

---
+++ lecture-19/app/src/main/java/org/haqnawaz/mclecture19fragments/MainActivity.java
@@ -0,0 +1,14 @@
+package org.haqnawaz.mclecture19fragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
